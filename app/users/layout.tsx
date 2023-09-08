6c22259060baa86862510f707375277e03591587
vinicius-sabadim <vinicius.sabadim@gmail.com>
2023-09-08 11:02:49 +0200

    Add Users component

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
 import Sidebar from '@/app/components/sidebar/Sidebar'
+import getUsers from '@/app/actions/getUsers'
+import UserList from '@/app/users/components/UserList'
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const users = await getUsers()
+
   return (
     <Sidebar>
+      <UserList items={users} />
       <div className='h-full'>{children}</div>
     </Sidebar>
   )
